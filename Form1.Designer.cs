c08f275fa8864bf17fee49cdfd1965d2dcce6eb5
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2022-02-18 11:16:33 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace NavegadorE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navegarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adelanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxUrls = new System.Windows.Forms.ComboBox();
+            this.buttonIr = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navegarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1393, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navegarToolStripMenuItem
+            // 
+            this.navegarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adelanteToolStripMenuItem,
+            this.atrasToolStripMenuItem,
+            this.homeToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.navegarToolStripMenuItem.Name = "navegarToolStripMenuItem";
+            this.navegarToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.navegarToolStripMenuItem.Text = "Navegar";
+            // 
+            // adelanteToolStripMenuItem
+            // 
+            this.adelanteToolStripMenuItem.Name = "adelanteToolStripMenuItem";
+            this.adelanteToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.adelanteToolStripMenuItem.Text = "Adelante";
+            this.adelanteToolStripMenuItem.Click += new System.EventHandler(this.adelanteToolStripMenuItem_Click);
+            // 
+            // atrasToolStripMenuItem
+            // 
+            this.atrasToolStripMenuItem.Name = "atrasToolStripMenuItem";
+            this.atrasToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.atrasToolStripMenuItem.Text = "Atras";
+            this.atrasToolStripMenuItem.Click += new System.EventHandler(this.atrasToolStripMenuItem_Click);
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.homeToolStripMenuItem.Text = "Home";
+            this.homeToolStripMenuItem.Click += new System.EventHandler(this.homeToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // comboBoxUrls
+            // 
+            this.comboBoxUrls.FormattingEnabled = true;
+            this.comboBoxUrls.Location = new System.Drawing.Point(24, 38);
+            this.comboBoxUrls.Name = "comboBoxUrls";
+            this.comboBoxUrls.Size = new System.Drawing.Size(851, 21);
+            this.comboBoxUrls.TabIndex = 1;
+            // 
+            // buttonIr
+            // 
+            this.buttonIr.Location = new System.Drawing.Point(913, 35);
+            this.buttonIr.Name = "buttonIr";
+            this.buttonIr.Size = new System.Drawing.Size(75, 23);
+            this.buttonIr.TabIndex = 2;
+            this.buttonIr.Text = "Ir";
+            this.buttonIr.UseVisualStyleBackColor = true;
+            this.buttonIr.Click += new System.EventHandler(this.buttonIr_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(24, 88);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(964, 475);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 638);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.buttonIr);
+            this.Controls.Add(this.comboBoxUrls);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navegarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adelanteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBoxUrls;
+        private System.Windows.Forms.Button buttonIr;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Guardar(string fileName, string texto)
+        {            
+            FileStream stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);            
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine(texto);            
+            writer.Close();
+        }
+
+
+        private void buttonIr_Click(object sender, EventArgs e)
+        {
+            string uri = "";
+
+            if (comboBoxUrls.SelectedItem != null)
+                uri = comboBoxUrls.SelectedItem.ToString();
+            if (comboBoxUrls.Text != null)
+                uri = comboBoxUrls.Text;
+
+            if (!uri.Contains("."))
+                uri = "https://www.google.com/search?q=" + uri;
+            if (!uri.Contains("https://"))
+                uri = "https://" + uri;
+
+            webBrowser1.Navigate(new Uri(uri));
+
+            int yaEsta = 0;
+
+            for (int i = 0; i < comboBoxUrls.Items.Count; i++)
+            {
+                if (uri == comboBoxUrls.Items[i].ToString())
+                    yaEsta++;
+            }
+
+            if (yaEsta == 0)
+            {
+                comboBoxUrls.Items.Add(uri);
+                Guardar("Historial.txt", uri);
+            }
+
+        }
+
+        private void adelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void atrasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+
+        private void Leer (string fileName)
+        {
+            
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            
+            while (reader.Peek() > -1)
+            {
+                //richTextBox1.AppendText(reader.ReadLine());
+                comboBoxUrls.Items.Add(reader.ReadLine());
+            }
+
+            reader.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Leer("Historial.txt");
+
+        }
+    }
+}
